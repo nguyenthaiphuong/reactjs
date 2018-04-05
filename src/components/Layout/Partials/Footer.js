a4706727f00baab5d1fe32da7301fee9834e6723
phuongnt <phuong.nt@neo-lab.vn>
2018-04-05 14:20:12 +0700

    init

---
+++ src/components/Layout/Partials/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    const copyRight = `© ${(new Date()).getFullYear()}`;
+    return (
+      <footer>
+      </footer>
+    );
+  }
+}
